464f271bd086c730e03c74cf077c582c1314ba1f
camilasanchezu <camila.sanchez.urresta@udla.edu.ec>
2024-10-08 09:24:59 -0500

    Creacion API

--- APISistemaVentaCS/SistemaVenta.IOC/Dependencia.cs
+++ APISistemaVentaCS/SistemaVenta.IOC/Dependencia.cs
@@ -9,6 +9,9 @@ using System.Text;
 using System.Threading.Tasks;
 using SistemaVenta.DAL.Repositorios.Contrato;
 using SistemaVenta.DAL.Repositorios;
+using SistemaVenta.Utility;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.BLL.Servicios;
 
 namespace SistemaVenta.IOC
 {
@@ -23,6 +26,20 @@ namespace SistemaVenta.IOC
 
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+
+
+            services.AddScoped<IRolService, RolService>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<IProductoService, ProductoService>();
+            services.AddScoped<IVentaService, VentaService>();
+            services.AddScoped<IMenuService, MenuService>();
+            services.AddScoped<IDashBoardService, DashBoardService>();
+
+
 
         }
     }
